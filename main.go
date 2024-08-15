568c181e2a010a5ff6946107b2c8f98c35378c39
henok-tesfu <henoktesf48@gmail.com>
2024-08-15 23:19:24 +0300

    reversed tested added

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
     "fmt"
-    "github.com/your-organization/basics-go/arrays"
+    "github.com/your-organization/basics-go/slices"
 )
 
 func main() {
     fmt.Println("Testing basics-go")
 
     // test all the packages here 
+	original := []int{1, 2, 3, 4, 5}
+	fmt.Println("Original:", original)
+	reversed :=	slices.ReverseElements(original)
+	fmt.Println("Reversed:", reversed)
 
-	
    
 }
--- slices/reverse.go
+++ slices/reverse.go
@@ -1,12 +1,13 @@
-package slice
+package slices
 
 
 // Reverse reverses the order of elements in a slice of ints.
 
-func ReverseElements(slice []int) {
+func ReverseElements(slice []int) []int {
  for start, end := 0, len(slice)-1; start < end; start, end = start+1, end-1 {
 
 	// swap the elements in the slice using the last element at the end with the start on the front
 	 slice[start], slice[end] = slice[end], slice[start]
  }
+ return slice
 }
