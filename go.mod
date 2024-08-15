81e3d0a13dc021ab952f0b8343c187963af60854
henok-tesfu <henoktesf48@gmail.com>
2024-08-15 22:02:01 +0300

    Initial setup of basics-go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/your-organization/basics-go
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "github.com/your-organization/basics-go/arrays"
+)
+
+func main() {
+    fmt.Println("Testing basics-go")
+
+    // test all the packages here 
+   
+}
