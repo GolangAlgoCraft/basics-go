b6a70b9737b6c2812d9d26755e65f6da40557458
henok-tesfu <henoktesf48@gmail.com>
2024-08-15 23:15:01 +0300

    reverse added

--- slice/reverse.go
+++ slice/reverse.go
@@ -1,3 +1,12 @@
 package slice
 
-	
+
+// Reverse reverses the order of elements in a slice of ints.
+
+func ReverseElements(slice []int) {
+ for start, end := 0, len(slice)-1; start < end; start, end = start+1, end-1 {
+
+	// swap the elements in the slice using the last element at the end with the start on the front
+	 slice[start], slice[end] = slice[end], slice[start]
+ }
+}
