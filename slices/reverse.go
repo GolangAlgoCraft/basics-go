d362a824b6a4dc6f900e032f8b05364673bc7516
Elnatal Debebe <elnataldebebe@gmail.com>
2024-08-16 10:26:25 +0300

    :white_check_mark: Adding unit test for the reverse slice function and removing testing data from main

--- slices/reverse.go
+++ slices/reverse.go
@@ -1,13 +1,11 @@
 package slices
 
-
 // Reverse reverses the order of elements in a slice of ints.
+func Reverse(slice []int) []int {
+	for start, end := 0, len(slice)-1; start < end; start, end = start+1, end-1 {
+		// swap the elements in the slice using the last element at the end with the start on the front
+		slice[start], slice[end] = slice[end], slice[start]
+	}
 
-func ReverseElements(slice []int) []int {
- for start, end := 0, len(slice)-1; start < end; start, end = start+1, end-1 {
-
-	// swap the elements in the slice using the last element at the end with the start on the front
-	 slice[start], slice[end] = slice[end], slice[start]
- }
- return slice
+	return slice
 }
