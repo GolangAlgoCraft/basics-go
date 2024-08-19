8c065478c28f7440a293770ba246a1e21db298ce
henok-tesfu <henoktesf48@gmail.com>
2024-08-19 22:59:56 +0300

    second largest hundle multiple types

--- slices/second_largest.go
+++ slices/second_largest.go
@@ -1,20 +1,30 @@
 package slices
-//returns the second largest element in a slice of ints.
 
-func SecondLargest(slice []int) int {
-	if len(slice) < 2 {
-		return 0
+// SecondLargest returns the second largest element in a slice of ints or floats.
+func SecondLargest[T int32 | int64 | float32 | float64](values []T) T {
+	if len(values) < 2 {
+		var zero T
+		// Return the zero value for the type if there aren't enough elements return default value
+		return zero 
 	}
-	firstLargest := slice[0]
-	SecondLargest := slice[0]
+	
+	firstLargest := values[0]
+	secondLargest := values[0]
 
-	for _,value :=range slice{
-		if firstLargest < value{
-			SecondLargest = firstLargest
+	for _, value := range values {
+		if value > firstLargest {
+			secondLargest = firstLargest
 			firstLargest = value
-		}else if SecondLargest < value && value != firstLargest{
-			SecondLargest = value
+		} else if value > secondLargest && value != firstLargest {
+			secondLargest = value
 		}
 	}
-	return SecondLargest
+	
+	// Handle edge case where all elements are the same
+	if firstLargest == secondLargest {
+		var zero T
+		return zero
+	}
+
+	return secondLargest
 }
