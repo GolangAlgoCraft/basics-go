a8dd333c7633856f47ed23a5ae0cc7fd41c926bc
Elnatal Debebe <elnataldebebe@gmail.com>
2024-08-20 11:18:04 +0300

    :white_check_mark: Update test cases for Sum and Reverse functions

--- slices/reverse.go
+++ slices/reverse.go
@@ -1,7 +1,7 @@
 package slices
 
-// Reverse reverses the order of elements in a slice of ints.
-func Reverse(slice []int) []int {
+// Reverse reverses the order of elements in a slice.
+func Reverse[T int | int32 | int64 | float32 | float64 | string](slice []T) []T {
 	for start, end := 0, len(slice)-1; start < end; start, end = start+1, end-1 {
 		// swap the elements in the slice using the last element at the end with the start on the front
 		slice[start], slice[end] = slice[end], slice[start]
--- slices/sum_test.go
+++ slices/sum_test.go
@@ -1,93 +1,47 @@
 package slices
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-
-	type testCase[T int | int32 | int64 | float32 | float64 | string] struct {
+	tests := []struct {
 		name     string
-		values   []T
-		expected T
+		values   interface{}
+		expected interface{}
+	}{
+		// test case int
+		{name: "Collection of 5 values", values: []int{1, 2, 3, 4, 5}, expected: 15},
+		{name: "Collection with one value", values: []int{1}, expected: 1},
+		{name: "Empty collection", values: []int{}, expected: 0},
+		// test case float64
+		{name: "Collection of 5 values", values: []float64{1.2, 2.3, 3.4, 4.5, 5.6}, expected: 17.0},
+		{name: "Collection with one value", values: []float64{1.2}, expected: 1.2},
+		{name: "Empty collection", values: []float64{}, expected: 0.0},
+		// test case for string
+		{name: "Collection of 5 values", values: []string{"the ", "quick ", "brown ", "fox ", "jumps"}, expected: "the quick brown fox jumps"},
+		{name: "Collection with one value", values: []string{"the"}, expected: "the"},
+		{name: "Empty collection", values: []string{}, expected: ""},
 	}
 
-	testCasesForInt := []testCase[int]{
-		{
-			name:     "Collection of 5 values",
-			values:   []int{1, 2, 3, 4, 5},
-			expected: 15,
-		},
-		{
-			name:     "Collection with one value",
-			values:   []int{1},
-			expected: 1,
-		},
-		{
-			name:     "Empty collection",
-			values:   []int{},
-			expected: 0,
-		},
-	}
-
-	for _, tc := range testCasesForInt {
-		t.Run(tc.name, func(t *testing.T) {
-			got := Sum(tc.values)
-			if got != tc.expected {
-				t.Errorf("got %d, want %d", got, tc.expected)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got interface{}
+			switch v := tt.values.(type) {
+			case []int:
+				got = Sum(v)
+			case []float64:
+				got = Sum(v)
+			case []string:
+				got = Sum(v)
+			default:
+				t.Fatalf("unsupported type %T", v)
 			}
-		})
-	}
 
-	testCasesForFloat := []testCase[float64]{
-		{
-			name:     "Collection of 5 values",
-			values:   []float64{1.2, 2.3, 3.4, 4.5, 5.6},
-			expected: 17.0,
-		},
-		{
-			name:     "Collection with one value",
-			values:   []float64{1.2},
-			expected: 1.2,
-		},
-		{
-			name:     "Empty collection",
-			values:   []float64{},
-			expected: 0,
-		},
-	}
-
-	for _, tc := range testCasesForFloat {
-		t.Run(tc.name, func(t *testing.T) {
-			got := Sum(tc.values)
-			if got != tc.expected {
-				t.Errorf("got %f, want %f", got, tc.expected)
+			if got != tt.expected {
+				t.Errorf("got %v, want %v", got, tt.expected)
 			}
-		})
-	}
 
-	testCasesForString := []testCase[string]{
-		{
-			name:     "Collection of 5 strings",
-			values:   []string{"the ", "quick ", "brown ", "fox ", "jumps"},
-			expected: "the quick brown fox jumps",
-		},
-		{
-			name:     "Collection with one string",
-			values:   []string{"the"},
-			expected: "the",
-		},
-		{
-			name:     "Empty collection",
-			values:   []string{},
-			expected: "",
-		},
-	}
-
-	for _, tc := range testCasesForString {
-		t.Run(tc.name, func(t *testing.T) {
-			got := Sum(tc.values)
-			if got != tc.expected {
-				t.Errorf("got %s, want %s", got, tc.expected)
-			}
 		})
 	}
 
